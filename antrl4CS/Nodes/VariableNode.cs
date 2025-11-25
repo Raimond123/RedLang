ecc60fd187c856fd3cc34db651e24ad27194789f
selinefeliz <92600472+selinefeliz@users.noreply.github.com>
2025-11-25 12:16:02 -0500

    Add VariableNode class for variable declarations

---
+++ antrl4CS/Nodes/VariableNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antrl4CS.Nodes
+{
+  public class VariableNode : DeclarationNode
+  {
+      //Creado en: VisitDeclare_stmt y VisitDecl_head (cuando es decl con posible inicializador).
+      public ExpressionNode? Expression; //? por si es opcional
+      
+  }
+}
