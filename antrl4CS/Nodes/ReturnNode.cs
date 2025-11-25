cc027b0cc968d992a0a5228c7cb1ebd662ccb9eb
selinefeliz <92600472+selinefeliz@users.noreply.github.com>
2025-11-25 12:13:46 -0500

    Add ReturnNode class for expression handling

---
+++ antrl4CS/Nodes/ReturnNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antrl4CS.Nodes
+{
+   public class ReturnNode : StatementNode
+  {
+      public ExpressionNode? Expression;
+  }
+}
