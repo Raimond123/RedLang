f75a73475f94bdaf164a90c8bbb71dd4e07bbc4d
selinefeliz <92600472+selinefeliz@users.noreply.github.com>
2025-11-25 12:12:58 -0500

    Add FunctionNode class for function declarations

---
+++ antrl4CS/Nodes/FunctionNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antrl4CS.Nodes
+{
+   public class FunctionNode : DeclarationNode
+  {
+      public bool IsEntry; //si es una funcion de entrada o no
+      public List<ParameterNode>? Parameters = [];
+      public BlockNode Body = null!;
+      
+  }
+}
